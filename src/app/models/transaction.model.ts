fcef19d7e0dc373dd637cfeeaa25877fb4245e63
naderkhiari <khiarinader@mahdia.r-iset.tn>
2025-05-13 21:55:09 +0100

    update

--- src/app/models/transaction.model.ts
+++ src/app/models/transaction.model.ts
@@ -2,9 +2,7 @@ export interface Transaction {
     id: number;
     date: string | Date;
     clientName: string;
-    type: string;
     amount: number;
     status: string;
-    paymentMethod: string;
     reference: string;
 }
